f434a658616286874ae467e7589d9ad66c913e5a
Oleg Chernykh <96500955+sqwolg@users.noreply.github.com>
2023-08-28 16:24:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+print("Привет! Это тестовый код на Python.")
+
+a = 5
+b = 3
+sum_result = a + b
+product_result = a * b
+
+print(f"Сумма чисел {a} и {b} равна {sum_result}.")
+print(f"Произведение чисел {a} и {b} равно {product_result}.")
